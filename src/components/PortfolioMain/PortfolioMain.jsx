c9fd7b3f3dd15fab1ee99369e94098ab87593e0a
K. M. Safwan Hassan <safwanhassan13@gmail.com>
2023-06-11 14:35:13 +0100

    adjusted responvise in the leading page.

--- src/components/PortfolioMain/PortfolioMain.jsx
+++ src/components/PortfolioMain/PortfolioMain.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// image
+import background from '../../assets/images/under-construction.png'
+
+// styles
+import styles from "./PortfolioMain.module.css";
+
 const PortfolioMain = () => {
     return (
-        <div>
-            
+        <div className={`${styles.background}`}>
+            <img src={background} alt="Construction" className={`${styles.backgroundImage}`}/>
         </div>
     );
 }
